d93878cac0520da3debdfda2cd559ba1727a1dde
Jonathan Frank <41275844+Jonathhhan@users.noreply.github.com>
2023-03-14 03:27:19 +0100

    Add files via upload

--- libs/openFrameworks/gl/ofShader.h
+++ libs/openFrameworks/gl/ofShader.h
@@ -26,19 +26,18 @@ typedef ofColor_<float> ofFloatColor;
 enum ofLogLevel: short;
 
 struct ofShaderSettings {
-    std::map<GLuint, of::filesystem::path> shaderFiles;
+    std::map<GLuint, std::filesystem::path> shaderFiles;
     std::map<GLuint, std::string> shaderSources;
     std::map<std::string, int> intDefines;
     std::map<std::string, float> floatDefines;
-    of::filesystem::path sourceDirectoryPath;
+    std::filesystem::path sourceDirectoryPath;
     bool bindDefaults = true;
 };
 
 class ofShader {
 
 	struct Source{
-		// FIXME: change source to of::filesystem::path
-		Source(GLuint type, const std::string & source, const of::filesystem::path & directoryPath)
+		Source(GLuint type, const std::string & source, const std::filesystem::path & directoryPath)
 			:type(type)
 			,source(source)
 			,directoryPath(directoryPath){}
@@ -48,7 +47,7 @@ class ofShader {
 		GLuint type;
 		std::string source;
 		std::string expandedSource;
-		of::filesystem::path directoryPath;
+		std::filesystem::path directoryPath;
 		std::map<std::string, int>   intDefines;
 		std::map<std::string, float> floatDefines;
 	};
@@ -61,20 +60,20 @@ public:
 	ofShader(ofShader && shader);
 	ofShader & operator=(ofShader && shader);
 
-	bool load(const of::filesystem::path& shaderName);
-	bool load(const of::filesystem::path& vertName, const of::filesystem::path& fragName, const of::filesystem::path& geomName="");
+	bool load(const std::filesystem::path& shaderName);
+	bool load(const std::filesystem::path& vertName, const std::filesystem::path& fragName, const std::filesystem::path& geomName="");
 #if !defined(TARGET_OPENGLES) && defined(glDispatchCompute)
-	bool loadCompute(const of::filesystem::path& shaderName);
+	bool loadCompute(const std::filesystem::path& shaderName);
 #endif
 
 #if !defined(TARGET_OPENGLES) || defined(TARGET_EMSCRIPTEN)
 	struct TransformFeedbackSettings {
-		std::map<GLuint, of::filesystem::path> shaderFiles;
+		std::map<GLuint, std::filesystem::path> shaderFiles;
 		std::map<GLuint, std::string> shaderSources;
 		std::vector<std::string> varyingsToCapture;
 		std::map<std::string, int> intDefines;
 		std::map<std::string, float> floatDefines;
-		of::filesystem::path sourceDirectoryPath;
+		std::filesystem::path sourceDirectoryPath;
 		bool bindDefaults = true;
 		GLuint bufferMode = GL_INTERLEAVED_ATTRIBS; // GL_INTERLEAVED_ATTRIBS or GL_SEPARATE_ATTRIBS
 	};
@@ -232,7 +231,7 @@ public:
 	// these methods create and compile a shader from source or file
 	// type: GL_VERTEX_SHADER, GL_FRAGMENT_SHADER, GL_GEOMETRY_SHADER_EXT etc.
 	bool setupShaderFromSource(GLenum type, std::string source, std::string sourceDirectoryPath = "");
-	bool setupShaderFromFile(GLenum type, const of::filesystem::path& filename);
+	bool setupShaderFromFile(GLenum type, const std::filesystem::path& filename);
 
 	// links program with all compiled shaders
 	bool linkProgram();
@@ -281,7 +280,7 @@ private:
 #endif
 
 	bool setupShaderFromSource(Source && source);
-	ofShader::Source sourceFromFile(GLenum type, const of::filesystem::path& filename);
+	ofShader::Source sourceFromFile(GLenum type, const std::filesystem::path& filename);
 	void checkProgramInfoLog();
 	bool checkProgramLinkStatus();
 	void checkShaderInfoLog(GLuint shader, GLenum type, ofLogLevel logLevel);
@@ -298,8 +297,8 @@ private:
 	/// @note			Include paths are always specified _relative to the including file's current path_
 	///	@note			Recursive #pragma include statements are possible
 	/// @note			Includes will be processed up to 32 levels deep
-	static std::string parseForIncludes( const std::string& source, const of::filesystem::path& sourceDirectoryPath = "");
-	static std::string parseForIncludes( const std::string& source, std::vector<std::string>& included, int level = 0, const of::filesystem::path& sourceDirectoryPath = "");
+	static std::string parseForIncludes( const std::string& source, const std::filesystem::path& sourceDirectoryPath = "");
+	static std::string parseForIncludes( const std::string& source, std::vector<std::string>& included, int level = 0, const std::filesystem::path& sourceDirectoryPath = "");
 
 	void checkAndCreateProgram();
 #ifdef TARGET_ANDROID
